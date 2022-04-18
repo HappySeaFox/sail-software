6a2ee4fc883444adecd7af5826e840c654b1073c
Dmitry Baryshev <dima8w@gmail.com>
2022-04-18 16:20:35 -0700

    Move navigation to a separate navbar

---
+++ public/nav.js
@@ -0,0 +1,26 @@
+﻿document.write(`
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/formats.html">Formats</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/benchmarks.html">Benchmarks</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/building.html">Building</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+`);
