8b3ebf52f35d8fe2c53ce205c602b84d13eb4577
Dmitry Baryshev <dima8w@gmail.com>
2022-04-19 14:14:50 -0700

    Added FAQ

--- public/nav.js
+++ public/nav.js
@@ -19,6 +19,9 @@
         <li class="nav-item">
           <a class="nav-link" href="/building.html">Building</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/faq.html">FAQ</a>
+        </li>
       </ul>
     </div>
   </div>
