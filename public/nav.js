1c28d75c750dd6211353c458924930f8c26f42e0
Dmitry Baryshev <dima8w@gmail.com>
2025-05-13 02:00:22 +0300

    Make flag smaller

--- public/nav.js
+++ public/nav.js
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-      <img src="ukraine.png" width="40" height="27" alt="Support Ukraine">
+      <img src="ukraine.png" width="30" height="20" alt="Support Ukraine">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
